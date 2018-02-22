08ebe15dafb3f3a641e7990e455a20c139c7eca9
claireisabelle <claire.bourdale@gmail.com>
2018-02-22 15:27:45 +0100

    Initialisation du plugin WP pour formulaire utilisateurs en front end

---
+++ README.md
@@ -0,0 +1,10 @@
+# PLUGIN WP CUSTOM POST TYPE
+
+## Description
+
+> Mini plugin for submit a custom form from front end
+
+## Credits
+
+* **Author**: Claire Bourdalé
+* **License**: GLP-v2 or later
---
+++ ga-sendrecipe.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: GA Submit Recipe from the Front End
+Plugin URI:
+Description: Allow the user to submit their recipe from the front end
+Version: 1.0
+Author: Claire Bourdalé
+License: GPL2
+License: URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
